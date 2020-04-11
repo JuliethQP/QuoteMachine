543dc2b92b38239a380bf5b7596ea88c671f1b40
Julieth <juliethquinchia@gmail.com>
2020-04-11 18:13:42 -0500

    Updates

---
+++ precache-manifest.4d941ea2f15aca73566f7b98604ac7a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d303a09b46ce0afd410f0e077879ef",
+    "url": "/QuoteMachine/index.html"
+  },
+  {
+    "revision": "f7601d2a5ccabeadf85b",
+    "url": "/QuoteMachine/static/css/main.eea2ae2d.chunk.css"
+  },
+  {
+    "revision": "b701ba4a01d7408610dd",
+    "url": "/QuoteMachine/static/js/2.a48f85a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QuoteMachine/static/js/2.a48f85a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7601d2a5ccabeadf85b",
+    "url": "/QuoteMachine/static/js/main.8f95d6df.chunk.js"
+  },
+  {
+    "revision": "2634cd78deaecf6aad92",
+    "url": "/QuoteMachine/static/js/runtime-main.569ab508.js"
+  }
+]);
